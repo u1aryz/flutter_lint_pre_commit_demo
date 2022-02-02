487ede743f2ab69cf2911bff7d828d9e14a79357
u1aryz <u1aryz.d@gmail.com>
2022-02-02 22:43:11 +0900

    Fixed to run only when there is a change in the dart files

--- tool/pre_commit.dart
+++ tool/pre_commit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 // ignore_for_file: avoid_print
@@ -7,11 +9,41 @@ main(List<String> args) {
 
 Future<int> _run(List<String> args) async {
   try {
-    var result = await Process.run('flutter', ['format', 'lib/', 'tool/']);
+    // exclude delete diff
+    final gitDiff = await Process.start('git', [
+      'diff',
+      '--cached',
+      '--name-only',
+      '--diff-filter=d',
+    ]);
+
+    final controller = StreamController<List<int>>();
+    final pipe = gitDiff.stdout.pipe(controller);
+    final dartFiles = [];
+
+    await for (var x in controller.stream
+        .transform(utf8.decoder)
+        .transform(const LineSplitter())) {
+      if (RegExp(r'\\*.dart+$').hasMatch(x)) {
+        dartFiles.add(x);
+      }
+    }
+
+    await pipe;
+
+    if (dartFiles.isEmpty) {
+      print('Skip because there is no change in the dart files.');
+      return 0;
+    }
+
+    var result = await Process.run(
+        'flutter', ['format', '--set-exit-if-changed', ...dartFiles]);
     print(result.stdout);
     if (result.exitCode != 0) {
       return result.exitCode;
     }
+
+    // look at analysis_options.yaml
     result = await Process.run('flutter', ['analyze']);
     return result.exitCode;
   } catch (e) {
